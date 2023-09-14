f470f6e2d706eeb512aff37106c26b9d75c2122d
usman khan <63948528+usmankhan706@users.noreply.github.com>
2023-09-14 11:53:52 +0500

    Initial commit

---
+++ src/app/lib/interceptors/server-error.interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+/**
+ * Interceptor that handles server errors.
+ *
+ * @param request The request object.
+ * @param next The next interceptor in the chain.
+ *
+ * @returns The next Observable.
+ */
+export const serverErrorInterceptor: HttpInterceptorFn = (request, next) => {
+    const router = inject(Router);
+
+    return next(request).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if ([HttpStatusCode.Unauthorized, HttpStatusCode.Forbidden].includes(error.status)) {
+                router.navigateByUrl('/auth/login');
+            }
+
+            return throwError(() => error);
+        }),
+    );
+};
